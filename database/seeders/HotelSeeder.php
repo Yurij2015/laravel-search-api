cc6046f3239d23a29d73ba5d36228d486faecc83
Yurij M <mokruy9999@gmail.com>
2023-03-18 15:02:36 +0100

    completed hotelSeeder"

--- database/seeders/HotelSeeder.php
+++ database/seeders/HotelSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Hotel;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class HotelSeeder extends Seeder
 {
@@ -12,6 +13,24 @@ class HotelSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('hotels')->truncate();
+        $csvData = fopen(base_path('public/files/data.csv'), 'rb');
+        $row = true;
+        while (($data = fgetcsv($csvData, 0, ',')) !== false) {
+            if (!$row) {
+                DB::table('hotels')->insert([
+                    'name' => $data['0'],
+                    'price' => $data['1'],
+                    'bedrooms' => $data['2'],
+                    'bathrooms' => $data['3'],
+                    'storeys' => $data['4'],
+                    'garages' => $data['5'],
+                    'created_at' => now(),
+                    'updated_at' => now()
+                ]);
+            }
+            $row = false;
+        }
+        fclose($csvData);
     }
 }
