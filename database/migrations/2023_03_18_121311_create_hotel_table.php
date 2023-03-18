4107ecc467711101035f8e2990ee65dd898241fe
Yurij M <mokruy9999@gmail.com>
2023-03-18 13:47:20 +0100

    change table name in migration

--- database/migrations/2023_03_18_121311_create_hotel_table.php
+++ database/migrations/2023_03_18_121311_create_hotel_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('hotel', static function (Blueprint $table) {
+        Schema::create('hotels', static function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->integer('price');
